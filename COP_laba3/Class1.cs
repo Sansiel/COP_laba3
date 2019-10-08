df8f335aca527e54750f6b77c9a1be1525b8be25
Sansiel <deviler.san@yandex.ru>
2019-10-08 14:14:30 +0400

    Добавьте файлы проекта.

---
+++ COP_laba3/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COP_laba3
+{
+    public class Class1
+    {
+    }
+}
